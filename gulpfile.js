d17220ef72897025d62f964dc97088aff269ad5e
Leo McKenzie <leo@solocomputerservices.com>
2016-01-10 17:56:42 -0500

    Initial Commit.

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    gutil = require('gulp-util'),
+    connect = require('gulp-connect');
+
+    // Path Variables
+ var   input = {
+        'sass' : './sass/**/*.scss',
+        'html' : './*.html',
+        'js' : './js/**/*.js'
+    },
+    output = {
+        'css' : './css/',
+        'html' : './'
+    };
+
+// SASS Processing Task
+gulp.task('sass', function() {
+   return gulp.src(['./sass/base.scss'])
+       .pipe(sass())
+       .pipe(gulp.dest(output.css))
+       .pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+   return gulp.src(['./js/**/*.js'])
+       .pipe(connect.reload());
+});
+
+// HTML Parsing and Server Reload
+gulp.task('html', function() {
+   return gulp.src(['./**/*.html'])
+       .pipe(connect.reload());
+});
+
+// Watchers
+gulp.task('watch', function() {
+   gulp.watch(['./**/*.html'], ['html']);
+    gulp.watch(['./css/**/*.css'], ['sass']);
+    gulp.watch(['./js/**/*.js'], ['js']);
+});
+
+// Initiate Server
+gulp.task('connect', function() {
+    connect.server({
+        root: './',
+        port: 8000,
+        livereload: true
+    });
+});
+
+// Que Task
+gulp.task('default', ['sass', 'js', 'html', 'watch', 'connect']);
+
